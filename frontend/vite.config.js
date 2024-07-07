5ab51d23728465e0b931012b7dd236b32c166e44
qrapidnow <qrapidnow@gmail.com>
2024-07-07 11:08:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-{
-  "version": 2,
-  "name": "mern-stack-project",
-  "builds": [
-    { "src": "backend/app.js", "use": "@vercel/node" },
-    { "src": "frontend/package.json", "use": "@vercel/static-build", "config": { "distDir": "frontend/build" } }
-  ],
-  "routes": [
-    { "src": "/api/(.*)", "dest": "/backend/app.js" },
-    { "src": "/(.*)", "dest": "/frontend/$1" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
