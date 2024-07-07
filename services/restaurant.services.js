a53840cfbb7b595603421e73915460bce2cfb2a4
qrapidnow <qrapidnow@gmail.com>
2024-07-07 10:03:57 +0530

    Add files via upload

---
+++ services/restaurant.services.js
@@ -0,0 +1,15 @@
+// services/restaurant.services.js
+const RestaurantModel = require('../model/restaurant.model');
+
+class RestaurantServices {
+    static async addRestaurant(details) {
+        try {
+            const newRestaurant = new RestaurantModel(details);
+            return await newRestaurant.save();
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = RestaurantServices;
